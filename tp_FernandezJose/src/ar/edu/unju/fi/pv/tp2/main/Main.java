8ebc97ed09e7868a0a2087de8556c2a62043743d
José Manuel Fernández <43698579@fi.unju.edu.ar>
2024-04-27 14:05:09 -0300

    Agregando main

--- tp_FernandezJose/src/ar/edu/unju/fi/pv/tp2/main/Main.java
+++ tp_FernandezJose/src/ar/edu/unju/fi/pv/tp2/main/Main.java
@@ -1,10 +1,13 @@
 package ar.edu.unju.fi.pv.tp2.main;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import ar.edu.unju.fi.pv.tp2.model.Producto;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		List<Producto> productos = new ArrayList<>();
 	}
-
 }
